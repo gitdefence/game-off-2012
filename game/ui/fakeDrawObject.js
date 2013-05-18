37ad7f354ed2d6ca73e495156f6865fc95e1ef6c
Quentin Brooks <yeerkkiller1@gmail.com>
2013-05-18 12:01:39 -0400

    Moved fakeDrawObject to the proper location, ui.

---
+++ game/ui/fakeDrawObject.js
@@ -0,0 +1,24 @@
+//An object which draws, but every time it draws it just calls a predefined draw function.
+//drawFnc should take a pen, a rect.
+//cachable defines if the drawFnc can be cached (we exposed redraw instead of draw)
+function FakeDrawObject(drawFnc, cachable) {
+    var self = this;
+
+    this.base = new BaseObj(self, 11);
+    this.tpos = new Rect(0, 0, 0, 0);
+
+    self.resize = function (rect) {
+        this.tpos = rect;
+    }
+
+    if (cachable) {
+        self.redraw = function (canvas) {
+            var pen = canvas.ctx();
+            drawFnc(pen, this.tpos);
+        }
+    } else {
+        self.draw = function (pen) {
+            drawFnc(pen, this.tpos);
+        }
+    }
+}
