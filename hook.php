aa484aff697d6aad52568b5cbc3d7147505565b2
crazy2be <crazy1be@gmail.com>
2013-05-21 17:28:23 -0400

    Be more trusting.

--- hook.php
+++ hook.php
@@ -1,26 +1,21 @@
 <?php
 $addr = $_SERVER['REMOTE_ADDR'];
-if ($addr !== '207.97.227.253' && $addr !== '50.57.128.197' && $addr !== '108.171.174.178' && $addr !== '50.57.231.61') {
-    exec("echo 'Access denied to $addr' >> ../gitdefence.log");
-    exit;
-}
 
 $payload = json_decode($_POST['payload']);
 $branch = explode('/', $payload->ref);
 $branch = $branch[2];
-echo "Branch: '$branch'";
-exec("echo 'pulling down $branch from $addr' >> gitdefence.log");
+$safe_branch = escapeshellarg($branch);
+echo "Branch: $safe_branch";
+exec("echo 'pulling down $safe_branch from $addr' >> gitdefence.log");
 
-if (is_dir($branch)) {
-    chdir($branch);
-    exec("git checkout $branch >> ../gitdefence.log 2>&1");
-    exec("git pull origin $branch >> ../gitdefence.log 2>&1");
+if (is_dir($branch) === TRUE) {
+	chdir($branch);
+	exec("git checkout $safe_branch >> ../gitdefence.log 2>&1");
+	exec("git checkout . >> ../gitdefence.log 2>&1");
+	exec("git pull origin $safe_branch >> ../gitdefence.log 2>&1");
 } else {
-    mkdir($branch);
-    chdir($branch);
-    exec("echo 'creating new repo for branch $branch' >> 
-    ../gitdefence.log");
-    exec("git clone git://github.com/gitdefence/game-off-2012.git . >> 
-    ../gitdefence.log 2>&1");
-    exec("git checkout $branch >> ../gitdefence.log 2>&1");
+	exec("echo 'creating new repo for branch $safe_branch' >> gitdefence.log");
+	exec("git clone git://github.com/gitdefence/game-off-2012 $safe_branch >> gitdefence.log 2>&1");
+	chdir($branch);
+	exec("git checkout $safe_branch >> ../gitdefence.log 2>&1");
 }
